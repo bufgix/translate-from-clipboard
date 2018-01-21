984820648899958ef35db4cbf9c52576035807f6
laszlokeuhl <laszlokuehl@yandex.com>
2018-01-21 18:25:36 +0300

    android(qpython, termux) support

--- tfc.py
+++ tfc.py
@@ -5,44 +5,55 @@
 
 __author__ = '@laszlokuehl'
 
-import json, requests, string
-import os, sys, signal, xerox
+import json, time, requests
+import os, re, sys, signal, string
 
 signal.signal(signal.SIGINT, lambda s, f: sys.exit())
 
-if sys.version_info[0] == 2:
+PY3 = sys.version_info[0] == 3      # --
+DROID = 'qpython' in sys.executable #  -- > :D
+TERMUX = 'termux' in sys.executable # --
+
+if DROID != True or TERMUX != True:
+    import xerox
+
+if PY3:
+    from urllib.parse import urlencode
+
+    if DROID:
+        import sl4a
+        droid = sl4a.Android()
+else:
     from urllib import urlencode
+
+    if DROID:
+        import androidhelper
+        droid = androidhelper.Android()
 
     reload(sys)
     sys.setdefaultencoding('utf-8')
-    letters = string.letters
-else:
-    from urllib.parse import urlencode
-    letters = string.ascii_letters
-
-letters += 'üÜıİöÖşŞğĞçÇ'
-
-default_config = {
-    'config': '{}/.sct.json'.format(os.path.expanduser('~')),
-    'dbpath': '{}/.dictorany.json'.format(os.path.expanduser('~')),
-    'from': 'en',
-    'to': 'tr'
-}
 
 class Translate(object):
+    API = "http://translate.googleapis.com/translate_a/single?client=gtx&sl={from_lang}&tl={to_lang}&dt=t&{query}"
+
+    if True in [TERMUX, DROID]:
+        userpath = '/sdcard/'
+    else:
+        userpath = os.path.expanduser('~')
+
+    default_config = {
+        'config': os.path.join(userpath, '.sct.json'),
+        'dbpath': os.path.join(userpath, '.dictorany.json'),
+        'from': 'en',
+        'to': 'tr'
+    }
+
     def __init__(self, configfile=None):
-        self.API = "http://translate.googleapis.com/translate_a/single?client=gtx&sl={from_lang}&tl={to_lang}&dt=t&{query}"
-
-        self.userpath = os.path.expanduser('~')
         self.config = self.parse_config(configfile)
-
         self.database = json.loads(open(self.config['dbpath'], 'r').read())
 
     def invert_dict(self, d):
         return dict([(v, k) for k, v in d.items()])
-
-    def get_word(self, string):
-        return ''.join([x for x in string if x in letters])
 
     def parse_config(self, configfile=None):
         if configfile == None:
@@ -52,19 +63,15 @@ class Translate(object):
             data = json.loads(open(configfile, 'r').read())
         else:
             with open(configfile, 'w') as f:
-                f.write(json.dumps(default_config, indent=4))
+                f.write(json.dumps(self.default_config, indent=4))
 
-            data = default_config
+            data = self.default_config
 
         if os.path.exists(data['dbpath']) != True:
             with open(data['dbpath'], 'w') as f:
                 f.write('{}')
 
         return data
-
-    def send_notify(self, msg):
-        command = 'notify-send -a "STC" -u "Low" "{msg}"'.format(msg=msg)
-        os.system(command)
 
     def insert_db(self, from_lang, to_lang, word, translated):
         if from_lang not in self.database:
@@ -86,9 +93,12 @@ class Translate(object):
             from_lang=from_lang, to_lang=to_lang, query=urlencode({
                 'q': word
             }))
-        translated = eval(requests.get(URL).text.replace('null', 'None'))[0][0][0]
 
-        return translated
+        try:
+            translated = eval(requests.get(URL).text.replace('null', 'None'))[0][0][0]
+            return translated
+        except:
+            return None
 
     def translate(self, from_lang, to_lang, word):
         try:
@@ -96,32 +106,69 @@ class Translate(object):
         except KeyError:
             translated = self.search_online(from_lang, to_lang, word)
 
+            if word.lower() == translated.lower():
+                return None
+
             if translated != None:
                 self.insert_db(from_lang, to_lang, word, translated)
 
             return translated
 
+class BaseApp(object):
+    def log(self, msg):
+        sys.stdout.write('[{time}] - {msg}\n'.format(time=time.ctime(), msg=msg))
+        sys.stdout.flush()
 
-class CliApp(Translate):
-    """
-    Cli app for Translate class
-    """
+    def send_notify(self, msg):
+        if DROID:
+            droid.makeToast(msg)
 
+        elif TERMUX:
+            pass
+
+        else:
+            command = 'notify-send -a "Translate From Clipboard" -u "Low" "{msg}"'.format(msg=msg)
+            os.system(command)
+
+    def set_clipboard(self, text=None):
+        if text == None:
+            text = ''
+
+        if DROID:
+            droid.setClipboard(text)
+        else:
+            xerox.copy(text)
+
+    def get_clipboard(self):
+        if DROID:
+            return droid.getClipboard().result
+        else:
+            return xerox.paste()
+
+class CliApp(BaseApp, Translate):
     def main(self):
-        xerox.copy('')
+        self.set_clipboard(text=None)
+
+        self.log('Translate From Clipboard: Started!')
+        self.log('Config: from {0} to {1}'.format(self.config['from'], self.config['to']))
 
         while True:
-            if xerox.paste() != '':
-                word = xerox.paste()
+            value = self.get_clipboard()
 
-                for word in word.split(' '):
-                    word = self.get_word(word)
+            if value != '':
+                for word in re.findall('\w+', value):
+                    translated = self.translate(self.config['from'], self.config['to'], word.lower())
 
-                    translated = self.translate(self.config['from'], self.config['to'], word)
-                    self.send_notify('* {0}: {1}'.format(word, translated))
+                    if translated != None:
+                        string = '{0}: {1}'.format(*list(map(lambda x: x.capitalize(), (word, translated))))
 
-                    xerox.copy('')
+                        self.send_notify(string)
+                        self.log(string)
 
+                    self.set_clipboard(text=None)
+
+class TkinterApp(BaseApp, Translate):
+    pass
 
 if __name__ == '__main__':
     app = CliApp()
